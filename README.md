b062ff965a4404f06bb63bb2b285ed317983d005
Zoinkh <zoinkberg@hotmail.com>
2025-01-09 19:26:24 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Bookstore_System with OOP, C# ,&.Net
+System_Design: https://www.figma.com/design/A3mCGwkKBCEKPa4jDr5J5z/BookStore_System-Design?node-id=0-1&t=gQ1PMURqmpzailVo-1
---
+++ book_store_system_gui/Form1.cs
@@ -0,0 +1,10 @@
+namespace book_store_system_gui
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
